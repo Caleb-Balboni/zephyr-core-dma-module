9860159cc5d9646c717c0525c108508a7f740f70
calebb <balboni.ca@northeastern.edu>
2026-02-02 22:11:41 -0500

    fix memcpy

--- driver/dma-library/core-dma.c
+++ driver/dma-library/core-dma.c
@@ -130,7 +130,6 @@ void async_receive_thread(const struct device* dev, void (*callback_func)(void*,
     if (atomic_get(&rx->seq) - 1 == atomic_get(&rx->ack)) {
       size_t copy_size = (cfg->chan_size / 2) - offsetof(struct dma_channel_info, data);
       void* data = (void*)rx->data;
-      memcpy(data, rx->data, copy_size);
       atomic_inc(&rx->ack);
       callback_func(data, user_data, copy_size);
       return;
